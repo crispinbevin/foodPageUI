f50149bfa1524b0458822e07901895db8ef88aa2
Crispin Bevin <crispinjithin@gmail.com>
2025-02-17 11:06:44 +0530

    initial

---
+++ foodpage/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="container-fluid login-section">
+      WORD
+    </div>
+  )
+}
+
+export default App
